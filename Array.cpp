3421a885fc478a670102c4f2ce789d13ddb42dc5
sanex7 <numasasa2008@gmail.com>
2024-12-09 17:45:27 +0200

    Added methods for Array

---
+++ Array.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime> 
+using namespace std;
+
+class Array {
+private:
+    int* arr;
+    size_t size;
+
+public:
+    Array(size_t n = 10) : size(n) {
+        arr = new int[size];
+        for (size_t i = 0; i < size; ++i) {
+            arr[i] = 0;
+        }
+    }
+
+    Array(size_t n, bool randomize) : size(n) {
+        arr = new int[size];
+        if (randomize) {
+            srand(static_cast<unsigned>(time(0)));
+            for (size_t i = 0; i < size; ++i) {
+                arr[i] = rand() % 100;
+            }
+        }
+    }
+
+    Array(size_t n, int min, int max) : size(n) {
+        arr = new int[size];
+        srand(static_cast<unsigned>(time(0)));
+        for (size_t i = 0; i < size; ++i) {
+            arr[i] = min + rand() % (max - min + 1);
+        }
+    }
+
+    Array(const Array& other) : size(other.size) {
+        arr = new int[size];
+        for (size_t i = 0; i < size; ++i) {
+            arr[i] = other.arr[i];
+        }
+    }
+
+    ~Array() {
+        delete[] arr;
+    }
+
+    void display() const {
+        for (size_t i = 0; i < size; ++i) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    void fillRandom(int min = 0, int max = 100) {
+        srand(static_cast<unsigned>(time(0)));
+        for (size_t i = 0; i < size; ++i) {
+            arr[i] = min + rand() % (max - min + 1);
+        }
+    }
+
+    void resize(size_t newSize) {
+        int* newArr = new int[newSize];
+        for (size_t i = 0; i < min(size, newSize); ++i) {
+            newArr[i] = arr[i];
+        }
+        for (size_t i = size; i < newSize; ++i) {
+            newArr[i] = 0;
+        }
+        delete[] arr;
+        arr = newArr;
+        size = newSize;
+    }
+
+    void sort() {
+        for (size_t i = 0; i < size - 1; ++i) {
+            for (size_t j = 0; j < size - i - 1; ++j) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    int minValue() const {
+        int minVal = arr[0];
+        for (size_t i = 1; i < size; ++i) {
+            if (arr[i] < minVal) {
+                minVal = arr[i];
+            }
+        }
+        return minVal;
+    }
+
+    int maxValue() const {
+        int maxVal = arr[0];
+        for (size_t i = 1; i < size; ++i) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+};
